3ddc0bd27323cf0eb024f8e87a28b0746b6d3094
kshetritej <tejghartikshetri@gmail.com>
2026-03-29 17:38:43 +0545

    chore: fix trip of the month card in mobile

--- components/featured-trip.tsx
+++ components/featured-trip.tsx
@@ -2,6 +2,7 @@ import { LucideCalendarClock, LucideCoins } from "lucide-react";
 import Image from "next/image";
 import { Badge } from "./ui/badge";
 import { Separator } from "./ui/separator";
+import Link from "next/link";
 
 const TripOfTheMonth = async () => {
   const res = await fetch(
@@ -10,51 +11,42 @@ const TripOfTheMonth = async () => {
   );
   const resJSON = await res.json();
 
-  const featured = resJSON?.data;
   const trip = resJSON?.data?.featuredTag?.activity;
 
   return (
-    // <SectionTemplate
-    //   className="w-full bg-primary/20"
-    //   title={
-    //     <p>
-    //       {featured?.featuredTag.name || "Hello world"}: {trip[0].title}
-    //     </p>
-    //   }
-    //   text={<p>{featured?.featuredTag.description || "Hello world"}</p>}
-    // >
-    // {
-    <div className="bg-primary/20 relative mx-auto py-12 p-2">
-      <div className="container mx-auto">
-        <Image
-          alt={trip[0].keywords[0] || trip[0].title || ""}
-          src={trip[0].images[0]}
-          height={720}
-          width={1280}
-          className="w-full object-cover rounded-2xl cursor-pointer"
-        />
-        {/*<div className="bg-linear-to-b from-white to-transparent absolute inset-0" />*/}
-        <div className="absolute md:bottom-24">
-          <div className="flex items-center justify-center  bg-white p-4 rounded-r-4xl">
-            <div className="flex flex-col justify-start items-start space-y-2">
-              <Badge>Trip of the Month</Badge>
-              <h2 className="font-black text-md md:text-5xl text-primary">
-                {trip[0].title.split(":")[0]}{" "}
-              </h2>
-              <div className="flex gap-1 mt-4">
-                <div className="md:text-2xl flex gap-2 items-start   text-primary">
-                  <LucideCalendarClock className="size-8" />
-                  <div>
-                    <p className="text-xs">Total Duration</p>
-                    <p>{trip[0].duration}</p>
+    <Link href={`/package/${trip[0].slug}`}>
+      <div className="bg-primary/20 relative mx-auto py-12 p-2">
+        <div className="container mx-auto">
+          <Image
+            alt={trip[0].keywords[0] || trip[0].title || ""}
+            src={trip[0].images[0]}
+            height={720}
+            width={1280}
+            className="w-full  h-120 md:h-full object-cover rounded-2xl cursor-pointer"
+          />
+          {/*<div className="bg-linear-to-b from-white to-transparent absolute inset-0" />*/}
+          <div className="absolute bottom-2 md:bottom-24 md:left-12">
+            <div className="flex items-center justify-center  bg-white p-4 rounded-4xl md:rounded-r-4xl">
+              <div className="flex flex-col justify-start items-start space-y-2 ">
+                <Badge>Trip of the Month</Badge>
+                <h2 className="font-black text-md md:text-5xl text-primary">
+                  {trip[0].title.split(":")[0]}{" "}
+                </h2>
+                <div className="flex gap-1 mt-4">
+                  <div className="md:text-2xl flex gap-2 items-start   text-primary">
+                    <LucideCalendarClock className="size-8" />
+                    <div>
+                      <p className="text-xs">Total Duration</p>
+                      <p>{trip[0].duration}</p>
+                    </div>
                   </div>
-                </div>
-                <Separator orientation="vertical" />
-                <div className="md:text-2xl flex gap-2 items-start pl-10 text-primary">
-                  <LucideCoins className="size-8" />
-                  <div>
-                    <p className="text-xs md:text-sm">Package Price</p>
-                    <p>USD {trip[0].price}</p>
+                  <Separator orientation="vertical" />
+                  <div className="md:text-2xl flex gap-2 items-start pl-10 text-primary">
+                    <LucideCoins className="size-8" />
+                    <div>
+                      <p className="text-xs md:text-sm">Package Price</p>
+                      <p>USD {trip[0].price}</p>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -62,9 +54,7 @@ const TripOfTheMonth = async () => {
           </div>
         </div>
       </div>
-    </div>
-    // }
-    // </SectionTemplate>
+    </Link>
   );
 };
 
