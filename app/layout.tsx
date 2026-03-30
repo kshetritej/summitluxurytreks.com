85453d52517578cae03b4672ea1c35d73c98088d
kshetritej <tejghartikshetri@gmail.com>
2026-03-30 16:44:48 +0545

    add: google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,8 +39,25 @@ export default function RootLayout({
                    })();
                  `}
         </Script>*/}
+        <Script id="gtm" strategy="afterInteractive">
+          {`<!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KNHGLHMM');</script>
+            <!-- End Google Tag Manager -->`}
+        </Script>
       </head>
       <body className={`${outfit.className} sans-serif antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KNHGLHMM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Navbar />
         {children}
         <Analytics />
