d58f1fdaca739392f2a229305830da8a9f436f4d
kshetritej <tejghartikshetri@gmail.com>
2026-04-05 17:18:55 +0545

    chore: add padding around logo

--- components/atoms/logo.tsx
+++ components/atoms/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function LogoComponent({ dark }: Readonly<{ dark?: boolean }>) {
   return (
-    <Link href="/" className="flex items-center gap-1 font-bold">
+    <Link href="/" className="flex items-center gap-1 font-bold p-2">
       <Image
         src={"/summit-luxury-logo-white.png"}
         alt="Summit Luxury Logo"
